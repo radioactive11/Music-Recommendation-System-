22157f9b77753d062d097968ebf03ee674f9dfab
Arijit Roy <roy.arijit2001@gmail.com>
2020-08-13 17:25:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import colorama
+
+colorama.init()
+
+print("loading files...\n")
+
+cosine_sim = np.load("sim.npy")
+
+print("files loaded\n\n")
+
+def get_song_id(inp_str):
+    ids = pd.read_csv("id.csv")
+    mask = ids["search"].str.contains(inp_str, regex=False, case=False)
+    search_results = ids[mask]
+    for i in range(len(search_results)):
+        song_id = search_results.iloc[i, 0]
+        song_name = search_results.iloc[i, 1]
+        song_artist = search_results.iloc[i, 2]
+        if(song_id < 40001):
+            print(str(song_id) + "\t" + song_name + " by " + song_artist)
+            print("\n")
+
+    song_id = int(input("Enter song ID: "))
+    return song_id
+
+
+def get_recommendations(idx, cosine_sim=cosine_sim):
+    
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the songs based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar songs
+    sim_scores = sim_scores[1:11]
+    sim_scores.sort()
+    # Get the song indices
+    song_index = [i[0] for i in sim_scores]
+    song_index = song_index
+    # Return the top 10 most similar songs
+    return song_index
+
+def display_recoms(recoms, idx):
+    song_id = pd.read_csv("id.csv")
+    print("Song recommendations for: " + song_id.iloc[idx, song_id.columns.get_loc("name")]+" by " + song_id.iloc[idx, song_id.columns.get_loc("artists")])
+    for i in recoms:
+        print(song_id.iloc[i, song_id.columns.get_loc("name")] + " by " +\
+            song_id.iloc[i, song_id.columns.get_loc("artists")])
+        print("\n")
+
+
+inp = 0
+while inp != -1:
+    inp_str = input("Enter song name: ")
+    song_id = get_song_id(inp_str)
+    recomms = get_recommendations(song_id)
+    display_recoms(recomms, song_id)
+    print('\n')
